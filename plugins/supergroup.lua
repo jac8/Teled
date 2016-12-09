0b09c92639c1f92b8ae6ddfcd968a18b41e9bbd5
jac8 <mrjac9@gmail.com>
2016-12-09 08:23:10 +0330

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -2238,7 +2238,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				text = "Chat Name: " ..string.gsub(msg.to.print_name, "_", " ").. " Chat ID: "..msg.to.id
+				text = "<b>Gp Name: " ..string.gsub(msg.to.print_name, "_", " ").. " Gp ID: "..msg.to.id</b>
 	reply_msg(msg.id, text, ok_cb, false)
 end
 		end
